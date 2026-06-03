4b78636b27912452d7104b168b47e26c1acb2645
ZoeyHuajun <zhangzoey100@gmail.com>
2026-06-03 16:11:49 -0600

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect
 import config
 # Create a Flask application instance
 app = Flask(__name__)
@@ -9,7 +9,19 @@ app.config.from_object(config)
 # Define a route for the home page127.0.0.1:5000 -view function
 @app.route('/')
 def home_page():
-    return 'Hello, World!'
+    return render_template('variable.html', hobby='看书')
+
+@app.get('/login')
+def login():
+    return f'Welcome to the page.'
+@app.get('/pub')
+def pub():
+    name = request.args.get('name')
+    if not name:
+        return redirect('/login')
+    else:
+        return f'Welcome, {name}!'
+
 if __name__ == '__main__':
     # Run the Flask application with new host and port settings
     app.run(debug=True, host='0.0.0.0', port=5050)
